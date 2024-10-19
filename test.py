28c96835fa703ef1e6397baab92c4a521225e9dc
Азамат <136832135+Azamat2023i@users.noreply.github.com>
2024-10-19 19:52:29 +0500

    Update test.py

--- test.py
+++ test.py
@@ -5,12 +5,30 @@ from addon import CustomMarkdown
 
 client = TelegramClient("my_account", api_id=api_id, api_hash=api_hash, system_version='4.16.30-vxCUSTOM')
 
+test_text = """_~_class Descriptor:_~_
+    **def \__get\__(self, instance, owner):**
+        __return instance._value__
+
+    **def \__set\__(self, instance, value):**
+        if value < 0:
+            raise ValueError("Значение должно быть положительным!")
+        instance._value = value
+
+_~_class MyClass:_~_
+    value = Descriptor()
+
+    def \__init\__(self, value):
+        self.value = value
+
+obj = MyClass(10)
+print(obj.value)  # 10
+obj.value = -5    # Ошибка: Значение должно быть положительным"""
+
 
 async def main():
     await client.start(phone=phone_number)
     client.parse_mode = CustomMarkdown()
-    await client.send_message(1359387573, "Это пример использования касторного markdown:\n**Жирный**\n__Курсив__\n~~Зачеркнутый~~\n`Капирующийся`\n```Код```\n_~_Подчеркнутый_~_\n||Спойлер||\n[Ссылка](https://github.com)\n[❤️](emoji/10002345) - кастомная эмоция (нужен тг премиум)\n[Цитата](blockquote/True) - Скрывающаяся (наглядно с большим обьемом текста)\n[Цитата](blockquote/False) - Полная (наглядно с большим обьемом текста)")
-    await client.run_until_disconnected()
+    await client.send_message(1359387573, "Это пример использования касторного markdown:\n**Жирный**\n__Курсив__\n~~Зачеркнутый~~\n`Капирующийся`\n```Код```\n_~_Подчеркнутый_~_\n||Спойлер||\n[Ссылка](https://github.com)\n[❤️](emoji/10002345) - кастомная эмоция (нужен тг премиум)\n\n\n[{test_text}](blockquote/False)\n\n\n[{test_text}](blockquote/True)")
 
 
 client.loop.run_until_complete(main())
