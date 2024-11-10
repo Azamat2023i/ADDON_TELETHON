c1f66c4af114484415783b273ff331adceeefeac
Азамат <136832135+Azamat2023i@users.noreply.github.com>
2024-11-10 22:52:21 +0500

    total change
    Changed Markdown to html

--- test.py
+++ test.py
@@ -5,30 +5,62 @@ from addon import CustomMarkdown
 
 client = TelegramClient("my_account", api_id=api_id, api_hash=api_hash, system_version='4.16.30-vxCUSTOM')
 
-test_text = """_~_class Descriptor:_~_
-    **def \__get\__(self, instance, owner):**
-        __return instance._value__
+test_text = """Это пример использования касторного markdown:
+<strong>Жирный</strong>
+<em>Курсив</em>
+<del>Зачеркнутый</del>
+<code>Капирующийся</code>
+<pre><code class='language-'>Код{}</code></pre>
+<u>Подчеркнутый</u>
+<spoiler>Спойлер</spoiler>
+<a href="https://github.com/">Ссылка</a>
+❤️ - кастомная эмоция (нужен тг премиум)
 
-    **def \__set\__(self, instance, value):**
-        if value < 0:
-            raise ValueError("Значение должно быть положительным!")
+
+<blockquote collapsed="False"><u>class Descriptor:</u>
+    <strong>def __get__(self, instance, owner):
+ </strong>       <em>return instance._value</em>
+
+    <strong>def __set__(self, instance, value):
+ </strong>       if value &lt; 0:
+            raise ValueError(&quot;Значение должно быть положительным!&quot;)
         instance._value = value
 
-_~_class MyClass:_~_
+<u>class MyClass:</u>
     value = Descriptor()
 
-    def \__init\__(self, value):
+    def __init__(self, value):
         self.value = value
 
 obj = MyClass(10)
 print(obj.value)  # 10
-obj.value = -5    # Ошибка: Значение должно быть положительным"""
+obj.value = -5    # Ошибка: Значение должно быть положительным</blockquote>
+
+
+<blockquote collapsed="True"><u>class Descriptor:</u>
+    <strong>def __get__(self, instance, owner):
+ </strong>       <em>return instance._value</em>
+
+    <strong>def __set__(self, instance, value):
+ </strong>       if value &lt; 0:
+            raise ValueError(&quot;Значение должно быть положительным!&quot;)
+        instance._value = value
+
+<u>class MyClass:</u>
+    value = Descriptor()
+
+    def __init__(self, value):
+        self.value = value
+
+obj = MyClass(10)
+print(obj.value)  # 10
+obj.value = -5    # Ошибка: Значение должно быть положительным</blockquote>"""
 
 
 async def main():
     await client.start(phone=phone_number)
     client.parse_mode = CustomMarkdown()
-    await client.send_message(1359387573, f"Это пример использования касторного markdown:\n**Жирный**\n__Курсив__\n~~Зачеркнутый~~\n`Капирующийся`\n```Код```\n_~_Подчеркнутый_~_\n||Спойлер||\n[Ссылка](https://github.com)\n[❤️](emoji/10002345) - кастомная эмоция (нужен тг премиум)\n\n\n%%#{test_text}%%#\n\n\n%%${test_text}%%$")
+    await client.send_message(1359387573, f"{test_text}", link_preview=False)
 
 
 client.loop.run_until_complete(main())
